1c2292b9fb8431b833dc181dca177b7554b476ac
frederic2ec <freddy.cho@hotmail.com>
2021-03-29 17:35:14 -0400

    Removed old typescript project

--- examples/typescript-old/config/database.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  models: "models/",
-  // development : { username: "", password: "", host: "", port: "", database: "" },
-  development: { uri: process.env.DATABASE_URL },
-  production: { uri: process.env.DATABASE_URL },
-};
--- examples/typescript-old/middlewares/auth.middleware.ts
+++
@@ -1,7 +0,0 @@
-module.exports.NotAuthed = (_: any, __: any, next: any) => {
-  next();
-};
-
-module.exports.Authed = (_: any, res: any, __: any) => {
-  res.send("This is the authed middleware");
-};

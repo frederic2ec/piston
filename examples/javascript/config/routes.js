2b20bddcf7f15ebc6ded5f79e5f698dcd7fa56f9
frederic2ec <freddy.cho@hotmail.com>
2021-03-29 17:25:06 -0400

    Removed old javascript project

--- examples/javascript/config/routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "get /": "Home/Index",
-  "get /test": "Home/Test",
-  "get /middleware": "Home/MiddlewarePage",
-  "/subrouter": {
-    "get /": "Subrouter/index",
-    "get /test": "Subrouter/test",
-  },
-};
--- examples/javascript/middlewares/auth.middleware.js
+++
@@ -1,7 +0,0 @@
-module.exports.NotAuthed = (req, res, next) => {
-  next();
-};
-
-module.exports.Authed = (req, res, next) => {
-  res.send("This is the authed middleware");
-};
